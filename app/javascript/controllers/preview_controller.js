3041041ca611d8643e2ba8af293071d58a0da52e
kernel_j <jenniferwong89@gmail.com>
2024-11-26 13:53:46 -0500

    feat: preview markdown

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -5,4 +5,26 @@ function parse(text) {
   return marked.parse(text);
 }
 
-export default class extends Controller {}
+export default class extends Controller {
+  static targets = ["input", "output"];
+  static values = { name: String };
+
+  connect() {
+    const value = this.inputTarget.value;
+
+    this.outputTarget.innerHTML = parse(value);
+  }
+
+  updateOutput() {
+    const value = this.inputTarget.value;
+
+    const nameToCheck = this.nameValue;
+
+    if (value.includes(nameToCheck)) {
+      this.outputTarget.classList.add("error");
+    } else {
+      this.outputTarget.classList.remove("error");
+    }
+    this.outputTarget.innerHTML = parse(value);
+  }
+}
